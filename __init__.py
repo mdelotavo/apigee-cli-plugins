0839f7a6694d1e0b3bee8fc7beb7f784baca194e
mdelotavo <46637675+mdelotavo@users.noreply.github.com>
2026-07-28 21:56:44 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
+# plugins = []
+
+# from .examples import examples
+# plugins.append('examples')
+
+# __all__ = []
+# for plugin in plugins:
+#     __all__.append(plugin)
 plugins = []
 
-from .examples import examples
-plugins.append('examples')
+from .plugin_b797e6c8fe8d47c8ba5c5689b2628c89 import examples
+plugins.append("examples")
 
-__all__ = []
-for plugin in plugins:
-    __all__.append(plugin)
+__all__ = plugins
